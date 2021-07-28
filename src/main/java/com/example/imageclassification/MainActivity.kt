f8c8cab7a54bf505c0267e1351a79dee0440acc2
Pratham <62070812+prathami1@users.noreply.github.com>
2021-07-28 17:59:06 -0400

    src for image classification

---
+++ src/main/java/com/example/imageclassification/MainActivity.kt
@@ -0,0 +1,96 @@
+package com.example.imageclassification
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.imageclassification.ml.MobilenetV110224Quant
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var select_image_button : Button
+    lateinit var make_prediction : Button
+    lateinit var img_view : ImageView
+    lateinit var text_view : TextView
+    lateinit var bitmap: Bitmap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        select_image_button = findViewById(R.id.button)
+        make_prediction = findViewById(R.id.button2)
+        img_view = findViewById(R.id.imageView2)
+        text_view = findViewById(R.id.textView)
+
+        val labels = application.assets.open("labels.txt").bufferedReader().use { it.readText() }.split("\n")
+
+        select_image_button.setOnClickListener(View.OnClickListener {
+            Log.d("mssg", "button pressed")
+            var intent : Intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+
+            startActivityForResult(intent, 100)
+        })
+
+        make_prediction.setOnClickListener(View.OnClickListener {
+            var resized = Bitmap.createScaledBitmap(bitmap, 224, 224, true)
+            val model = MobilenetV110224Quant.newInstance(this)
+
+            var tbuffer = TensorImage.fromBitmap(resized)
+            var byteBuffer = tbuffer.buffer
+
+// Creates inputs for reference.
+            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.UINT8)
+            inputFeature0.loadBuffer(byteBuffer)
+
+// Runs model inference and gets result.
+            val outputs = model.process(inputFeature0)
+            val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+            var max = getMax(outputFeature0.floatArray)
+
+            text_view.setText(labels[max])
+
+// Releases model resources if no longer used.
+            model.close()
+        })
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        img_view.setImageURI(data?.data)
+
+        var uri : Uri ?= data?.data
+        bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
+    }
+
+    fun getMax(arr:FloatArray) : Int{
+        var ind = 0;
+        var min = 0.0f;
+
+        for(i in 0..1000)
+        {
+            if(arr[i] > min)
+            {
+                min = arr[i]
+                ind = i;
+            }
+        }
+        return ind
+    }
+}
